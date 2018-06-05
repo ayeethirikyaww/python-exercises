efcc4a00956e0d53bc150870b600e56d2c041e89
ayeethirikyaww <atkyaw.comp@gmail.com>
2018-06-05 21:42:38 +0630

    add ex6.py

---
+++ ex6.py
@@ -0,0 +1,22 @@
+types_of_people=10
+x="There are %d types of people." %types_of_people
+
+binary="binary"
+do_not= "don't"
+y="Those who know %s and those who %s." %(binary,do_not)
+
+print(x)
+print(y)
+
+print("I said:%r" %x)
+print("I also said:%r" %y)
+
+hilarious=False
+joke_evaluation="Isn't that joke so funny? {}"
+
+print(joke_evaluation.format(hilarious))
+
+w="This is the left side of..."
+e="a string with a right side."
+
+print(w + e)
