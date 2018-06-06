a44b768c49c60741ae57ec6c2d7bc022d2df023e
ayeethirikyaww <atkyaw.comp@gmail.com>
2018-06-06 17:49:46 +0630

    add ex8.py

--- ex8.py
+++ ex8.py
@@ -4,5 +4,5 @@ print formatter %(1 ,2 ,3 ,4 )
 print formatter %("One","two","three","four")
 print formatter %(True ,False ,True ,False)
 print formatter %(formatter,formatter,formatter,formatter)
-print formatter.format("Try your","Own text here","Maybe a poen","Or a song about fear")
+print formatter %("Try your","Own text here","Maybe a poen","Or a song about fear")
 
