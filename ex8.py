40600a0edea398641d185df9a1bc9efefee6e48f
ayeethirikyaww <atkyaw.comp@gmail.com>
2018-06-07 19:11:32 +0630

    add ex8.py

--- ex8.py
+++ ex8.py
@@ -1,8 +1,13 @@
-formatter="%r %r %r %r"
+formatter="{} {} {} {}"
 
-print formatter %(1 ,2 ,3 ,4 )
-print formatter %("One","two","three","four")
-print formatter %(True ,False ,True ,False)
-print formatter %(formatter,formatter,formatter,formatter)
-print formatter %("Try your","Own text here","Maybe a poen","Or a song about fear")
+print(formatter.format(1 ,2 ,3 ,4 ))
+print(formatter.format("One","two","three","four"))
+print(formatter.format(True ,False ,True ,False))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format(
+        "Try your",
+        "Own text here",
+        "Maybe a poen",
+        "Or a song about fear"
+        ))
 
