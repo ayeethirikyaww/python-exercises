5fc5e60644fe78919d7a33f576b13c6f3a718a0c
ayeethirikyaww <atkyaw.comp@gmail.com>
2018-06-07 18:51:06 +0630

    add ex6.py

--- ex6.py
+++ ex6.py
@@ -1,15 +1,15 @@
 types_of_people=10
-x="There are %d types of people." %types_of_people
+x=f"There are {types_of_people} types of people."
 
 binary="binary"
 do_not= "don't"
-y="Those who know %s and those who %s." %(binary,do_not)
+y=f"Those who know {binary} and those who {do_not}."
 
 print(x)
 print(y)
 
-print("I said:%r" %x)
-print("I also said:%r" %y)
+print(f"I said:{x}")
+print(f"I also said:'{y}'")
 
 hilarious=False
 joke_evaluation="Isn't that joke so funny? {}"
